ac35aeb543f0827368407cffac22f02661ed7c14
rightsecond2 <jwh8712@naver.com>
2019-09-30 15:12:53 +0900

    123

--- test123/src/test/test.java
+++ test123/src/test/test.java
@@ -3,13 +3,10 @@ package test;
 public class test {
 	String name = "adcc";
 	String ename = "ee";
-<<<<<<< HEAD
 	String aname="aa";
-=======
 	int i =123;
 	public void asd() {
 		
 	}
 	String ddd = null;
->>>>>>> refs/heads/ms
 }
